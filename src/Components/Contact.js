8e058ef31053cd2f3d04d9a804ce985dedc30de4
Kyle McReynolds <53325780+kyle-mcr@users.noreply.github.com>
2020-09-21 16:27:20 -0700

    Update Contact.js

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,18 +1,19 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
-class Contact extends Component { 
-  render() {
+const Contact = ({ data }) => {
+   const [url, setUrl] = useState('mailto:test@example.com?subject=subject&body=body');
+   const [name, setName] = useState('');
+   const [subject, setSubject] = useState('');
+   const [email, setEmail] = useState('');
+   const [message, setMessage] = useState('');
 
-    if(this.props.data){
-      var name = this.props.data.name;
-      var street = this.props.data.address.street;
-      var city = this.props.data.address.city;
-      var state = this.props.data.address.state;
-      var zip = this.props.data.address.zip;
-      var phone= this.props.data.phone;
-      var email = this.props.data.email;
-      var message = this.props.data.contactmessage;
+   console.log(data)
+
+    const handleClick = (e) => {
+       e.preventDefault();
+      window.open(`mailto:${email}?subject=${subject}&body=${name}: ${message}`);
     }
+    
 
     return (
       <section id="contact">
@@ -27,29 +28,73 @@ class Contact extends Component {
 
             <div className="ten columns">
 
-                  <p className="lead">{message}</p>
+                  <p className="lead">{data?.message}</p>
 
             </div>
 
          </div>
-      
 
-            <aside className="contact_box">
+         <div className="row">
+            <div className="eight columns">
+
+               <form id="contactForm" name="contactForm">
+					<fieldset>
+
+                  <div>
+						   <label htmlFor="contactName">Name <span className="required">*</span></label>
+						   <input value={name} type="text" defaultValue="" size="35" id="contactName" name="contactName" onChange={e => setName(e.target.value)}/>
+                  </div>
+
+                  <div>
+						   <label htmlFor="contactEmail">Email <span className="required">*</span></label>
+						   <input value={email} type="text" defaultValue="" size="35" id="contactEmail" name="contactEmail" onChange={e=> setEmail(e.target.value)}/>
+                  </div>
+
+                  <div>
+						   <label htmlFor="contactSubject">Subject</label>
+						   <input value={subject} type="text" defaultValue="" size="35" id="contactSubject" name="contactSubject" onChange={e => setSubject(e.target.value)}/>
+                  </div>
+
+                  <div>
+                     <label htmlFor="contactMessage">Message <span className="required">*</span></label>
+                     <textarea value={message} onChange={e => setMessage(e.target.value)} cols="50" rows="15" id="contactMessage" name="contactMessage"></textarea>
+                  </div>
+
+                  <div>
+                     <button type='submit' onClick={handleClick} className="submit">Submit</button>
+                     <span id="image-loader">
+                        <img alt="" src="images/loader.gif" />
+                     </span>
+                  </div>
+					</fieldset>
+				   </form>
+
+           <div id="message-warning"> Error boy</div>
+				   <div id="message-success">
+                  <i className="fa fa-check"></i>Your message was sent, thank you!<br />
+				   </div>
+           </div>
+
+
+            <aside className="four columns footer-widgets">
                <div className="widget widget_contact">
 
-					   <h4>Address and Phone Number</h4>
+					   <h4>Address and Phone</h4>
 					   <p className="address">
-						   {name}<br />
-						   {street} <br />
-						   {city}, {state} {zip}<br />
-						   <span>{phone}</span>
+						   {data?.name}<br />
+						   {data?.address.street} <br />
+						   {data?.address.city}, {data?.address.state} {data?.address.zip}<br />
+						   <span>{data?.phone}</span>
 					   </p>
 				   </div>
 
+               <div className="widget widget_tweets">
+
+		         </div>
             </aside>
+      </div>
    </section>
     );
-  }
 }
 
 export default Contact;
